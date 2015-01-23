317b5836e64f06e5c74e041d646502e8ce18b179
Daniel Kelley <magn3t@gmail.com>
2015-01-23 10:41:29 -0600

    Get mobile menu animating working

--- js/high5.js
+++ js/high5.js
@@ -2,10 +2,13 @@
 
 $( function() {
    $('.menu-button').click( function(event) {
+       $('.nav').toggleClass("hidden");
+       /*
        if ($('.nav').css("display") === "none") {
            $('.nav').css("display", "block");
        } else {
            $('.nav').css("display", "none");
        }
+       */
    });
 });
