a1877b4b508d3c04ed1a91d4c71bbb9cdfbebee4
Daniel Kelley <dkelley42@gmail.com>
2015-02-02 13:09:06 -0600

    Incorporate standing ovation changes

--- js/standing_ovation.js
+++ js/standing_ovation.js
@@ -1,28 +1,24 @@
 // standing_ovation.js
 
-function save() {
-    var nameValue = document.getElementById('usr').value;
-    var levelValue = document.getElementById('level').value;
-    localStorage.setItem('name', nameValue);
-    localStorage.setItem('level', levelValue);
-    retrieve();
+function clickcall(){
+    var user = $('#usr').val();
+    var comment = $('#comment').val();
+    var level = $('#level').val();
+    var reason = $('#reason option:selected').val();
+
+    if (typeof(Storage) != "undefined") {
+        localStorage.setItem('name', user);
+        localStorage.setItem('comment', comment);
+        localStorage.setItem('level', level);
+        localStorage.setItem('reason', reason);
+
+        // Retrieve
+        var strname = localStorage.getItem('name');
+        var strlevel = localStorage.getItem('level');
+        var strreason = localStorage.getItem('reason');
+        var strcomment = localStorage.getItem('comment');
+        alert("Resource " + strname+ " has been nominated for the standing ovation "+ strlevel+ " with comments as follows: "+ strcomment);
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
 }
-
-var str_name;
-var str_level;
-
-function retrieve() {
-     str_name = localStorage.getItem('name');
-     str_level = localStorage.getItem('level');
-     window.alert("\t Standing Applause Award Summary \n\n"
-     + "\t Employee Name: "
-     + '\t ' + str_name + ' \n'
-     + "\t Award Level: "
-     + '\t ' + str_level + '\n');
-}
-
-$( function () {
-   $("form").submit( function(event) {
-      save(); 
-   });
-});
